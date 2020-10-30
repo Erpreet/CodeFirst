d48efcd05bb10d4b63831b412812559932877cac
Erpreet <preetkaurchd15@gmail.com>
2020-10-30 11:39:06 -0600

    Program.cs, Vehicle.cs, Manufacturer.cs, DealershipContext.cs

---
+++ EntityFramework_4Point2/Models/Manufacturer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace EntityFramework_4Point2.Models
+{
+    [Table("manufacturer")]
+    class Manufacturer
+    {
+
+        public Manufacturer()
+        {
+            Vehicles = new HashSet<Vehicle>();
+        }
+        [Key]
+        [Column("ID", TypeName = "int(10)")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        public int ID { get; set; }
+
+        [Column(TypeName = "varchar(30)")]
+
+        public string Name { get; set; }
+
+        [InverseProperty(nameof(Vehicle.Manufacturer))]
+        public virtual ICollection<Vehicle> Vehicles { get; set; }
+        
+    }
+}
--- EntityFramework_4Point2/Models/Vehicle.cs
+++ EntityFramework_4Point2/Models/Vehicle.cs
@@ -2,11 +2,35 @@
 using System.Collections.Generic;
 using System.Text;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace EntityFramework_4Point2.Models
 {
     [Table("vehicle")]
     class Vehicle
     {
+        [Key]
+        [Column("ID",TypeName ="int(10)")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        public int ID { get; set; }
+
+        [Column(TypeName = "varchar(30)")]
+        public string ManufacturerID { get; set; }
+
+        [Column(TypeName = "varchar(30)")]
+        public string Model { get; set; }
+
+        [Column(TypeName = "int(10)")]
+        public int ModelYear { get; set; }
+
+        [Column(TypeName = "varchar(30)")]
+        public string Colour { get; set; }
+
+        [ForeignKey(nameof(ManufacturerID))]
+        [InverseProperty(nameof(Models.Manufacturer.Vehicles))]
+        public virtual Manufacturer Manufacturer { get; set; }
+
+
     }
 }
