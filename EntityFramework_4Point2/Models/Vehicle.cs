c76c99f01025fd193ac0ff9a9938286311a580d5
Erpreet <preetkaurchd15@gmail.com>
2020-10-30 09:30:31 -0600

    Basic Commit

---
+++ EntityFramework_4Point2/Models/Vehicle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EntityFramework_4Point2.Models
+{
+    [Table("vehicle")]
+    class Vehicle
+    {
+    }
+}
---
+++ EntityFramework_4Point2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace EntityFramework_4Point2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
